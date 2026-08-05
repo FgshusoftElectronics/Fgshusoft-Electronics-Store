46e16a370a3fcc7c6586cc2a713606ca3ad16069
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 20:34:12 +0100

    Update router.js

--- admin/assets/js/router.js
+++ admin/assets/js/router.js
@@ -1,40 +1,26 @@
+import {showLoader, hideLoader} from "./loader.js";
+
 export async function navigate(module){
     showLoader();
     try{
-        // Load HTML
         const response = await fetch(
             `modules/${module}/${module}.html`
         );
+        if(!response.ok){
+            throw new Error(
+                `${module}.html not found`
+            );
+        }
         const html = await response.text();
-        document
-        .getElementById("content")
-        .innerHTML = html;
-        // Load CSS
-        const css =
-        document.createElement("link");
-        css.rel="stylesheet";
-        css.href=
-        `modules/${module}/${module}.css`;
-        document.head.appendChild(css);
-        // Load JS
-        const script =
-        document.createElement("script");
-        script.type="module";
-        script.src=
-        `modules/${module}/${module}.js`;
-        document.body.appendChild(script);
+        document.getElementById("content").innerHTML = html;
     }
     catch(error){
-        document.getElementById("content").innerHTML=`
+        console.error(error);
+        document.getElementById("content").innerHTML = `
         <div class="alert alert-danger">
-        <i class="fa-solid fa-triangle-exclamation"></i>
-        Failed loading module:
-        ${module}
+        ${error.message}
         </div>
         `;
-        console.error(error);
     }
-
     hideLoader();
-
 }
