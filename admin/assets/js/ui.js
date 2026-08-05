b610ff1f9181d9ea02b96c4242efe76c05021083
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 20:45:12 +0100

    Update ui.js

--- admin/assets/js/ui.js
+++ admin/assets/js/ui.js
@@ -1,22 +1,42 @@
+import { navigate } from "./router.js";
+
+
 export async function loadSidebar() {
 
-    const response = await fetch("templates/sidebar.html");
+    const response = await fetch(
+        "templates/sidebar.html"
+    );
 
     document.getElementById("sidebar").innerHTML =
         await response.text();
 
+
     document
-        .querySelectorAll("#sidebar a[data-page]")
-        .forEach(link => {
+    .querySelectorAll("#sidebar a[data-page]")
+    .forEach(link => {
 
-            link.addEventListener("click", e => {
+        link.addEventListener("click", e => {
 
-                e.preventDefault();
+            e.preventDefault();
 
-                navigate(link.dataset.page);
-
-            });
+            navigate(link.dataset.page);
 
         });
+
+    });
+
+}
+
+
+
+export async function loadTopbar() {
+
+    const response = await fetch(
+        "templates/topbar.html"
+    );
+
+
+    document.getElementById("topbar").innerHTML =
+        await response.text();
 
 }
