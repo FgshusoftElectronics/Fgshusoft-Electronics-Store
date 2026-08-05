76e62cead221148e2463d5a093eaefcd5df0e56a
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 20:08:38 +0100

    Update products.js

--- admin/modules/products/products.js
+++ admin/modules/products/products.js
@@ -1,1 +1,160 @@
+const products = [
 
+{
+id:1,
+
+name:"ESP32 Development Board",
+
+category:"IoT",
+
+price:"5000 FCFA",
+
+stock:25,
+
+image:"../../images/esp32.jpg"
+
+},
+
+
+{
+id:2,
+
+name:"Arduino UNO R3",
+
+category:"Microcontroller",
+
+price:"7000 FCFA",
+
+stock:15,
+
+image:"../../images/arduino.jpg"
+
+},
+
+
+{
+id:3,
+
+name:"DHT11 Sensor",
+
+category:"Sensors",
+
+price:"1500 FCFA",
+
+stock:40,
+
+image:"../../images/dht11.jpg"
+
+}
+
+];
+
+
+
+function renderProducts(){
+
+
+const table =
+document.getElementById("productsTable");
+
+
+table.innerHTML="";
+
+
+
+products.forEach(product=>{
+
+
+table.innerHTML += `
+
+
+<tr>
+
+
+<td>
+
+<img src="${product.image}"
+
+width="50"
+
+height="50"
+
+class="rounded">
+
+</td>
+
+
+
+<td>
+
+<strong>
+
+${product.name}
+
+</strong>
+
+</td>
+
+
+
+<td>
+
+<span class="badge bg-info">
+
+${product.category}
+
+</span>
+
+</td>
+
+
+
+<td>
+
+${product.price}
+
+</td>
+
+
+
+<td>
+
+${product.stock}
+
+</td>
+
+
+
+<td>
+
+
+<button class="btn btn-sm btn-warning">
+
+<i class="fa-solid fa-pen"></i>
+
+</button>
+
+
+<button class="btn btn-sm btn-danger">
+
+<i class="fa-solid fa-trash"></i>
+
+</button>
+
+
+</td>
+
+
+</tr>
+
+
+`;
+
+});
+
+
+}
+
+
+
+renderProducts();
