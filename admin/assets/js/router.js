fd5a5be543a19c664565aa35ea72df46bc9b6fc4
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 20:13:01 +0100

    Update router.js

--- admin/assets/js/router.js
+++ admin/assets/js/router.js
@@ -1,16 +1,39 @@
 export async function navigate(module){
-
     showLoader();
-
-    const response =
-    await fetch(`modules/${module}/${module}.html`);
-
-    const html =
-    await response.text();
-
-    document
-    .getElementById("content")
-    .innerHTML = html;
+    try{
+        // Load HTML
+        const response = await fetch(
+            `modules/${module}/${module}.html`
+        );
+        const html = await response.text();
+        document
+        .getElementById("content")
+        .innerHTML = html;
+        // Load CSS
+        const css =
+        document.createElement("link");
+        css.rel="stylesheet";
+        css.href=
+        `modules/${module}/${module}.css`;
+        document.head.appendChild(css);
+        // Load JS
+        const script =
+        document.createElement("script");
+        script.type="module";
+        script.src=
+        `modules/${module}/${module}.js`;
+        document.body.appendChild(script);
+    }
+    catch(error){
+        document.getElementById("content").innerHTML=`
+        <div class="alert alert-danger">
+        <i class="fa-solid fa-triangle-exclamation"></i>
+        Failed loading module:
+        ${module}
+        </div>
+        `;
+        console.error(error);
+    }
 
     hideLoader();
 
