522032b77b53be007d8c06eb270df2201cf5e37e
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 19:21:06 +0100

    Update login.js

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,1 +1,52 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit",(e)=>{
 
+e.preventDefault();
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+// Temporary login
+
+if(
+email==="admin@fgshusoft.com" &&
+password==="123456"
+){
+
+Swal.fire({
+
+icon:"success",
+
+title:"Welcome!",
+
+text:"Login successful.",
+
+timer:1500,
+
+showConfirmButton:false
+
+}).then(()=>{
+
+location.href="dashboard.html";
+
+});
+
+}else{
+
+Swal.fire({
+
+icon:"error",
+
+title:"Login Failed",
+
+text:"Incorrect email or password."
+
+});
+
+}
+
+});
