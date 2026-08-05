f836448c6c335ce24802abbf7ab7645ab93763cd
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-05 23:49:07 +0100

    Update loader.js

--- admin/assets/js/loader.js
+++ admin/assets/js/loader.js
@@ -1,20 +1,13 @@
 export function showLoader(){
 
-    const loader = document.getElementById("loader");
-
-    if(loader){
-        loader.style.display = "flex";
-    }
+    document.getElementById("loader")
+    .style.display = "flex";
 
 }
 
-
 export function hideLoader(){
 
-    const loader = document.getElementById("loader");
-
-    if(loader){
-        loader.style.display = "none";
-    }
+    document.getElementById("loader")
+    .style.display = "none";
 
 }
