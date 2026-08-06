e328ad75c9789b1a4edda1e2994f02c7e407fe69
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 02:40:26 +0100

    Update app.js

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -1,24 +1,57 @@
+import { checkAuth } from "./firebase-auth.js";
+
+import {
+    loadSidebar,
+    loadTopbar,
+    loadFooter
+} from "./ui.js";
+
+import {
+    navigate
+} from "./router.js";
+
+
+
+window.addEventListener(
+"DOMContentLoaded",
+async ()=>{
+
+
 try{
 
-    await checkAuth();
 
-    loadSidebar();
+await checkAuth();
 
-    loadTopbar();
 
-    loadFooter();
+await loadSidebar();
 
-    navigate("dashboard");
+await loadTopbar();
+
+await loadFooter();
+
+
+navigate("dashboard");
+
 
 }
+
 
 catch(e){
 
-    alert(
-        "Error: " + e.message
-    );
 
-    window.location.href =
-    "login.html";
+console.error(e);
+
+
+alert(
+"Error: " + e.message
+);
+
+
+window.location.href =
+"login.html";
+
 
 }
+
+
+});
