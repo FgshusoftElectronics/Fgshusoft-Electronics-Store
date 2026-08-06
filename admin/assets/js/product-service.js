828e098e65385f75c4054246ba6cd2ab41062083
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 13:58:20 +0100

    Update product-service.js

--- admin/assets/js/product-service.js
+++ admin/assets/js/product-service.js
@@ -8,7 +8,7 @@ serverTimestamp
 }
 from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
+ 
 import {db} from "./firebase.js";
 
 const productsRef = collection( db, "products" );
