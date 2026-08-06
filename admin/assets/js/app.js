df664f684077fac6a56bca76a209a2938413629c
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 11:20:02 +0100

    Update app.js

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -15,18 +15,17 @@ window.addEventListener(
 async ()=>{
 try{
 //await checkAuth();
-
+alert( "Step 1" )
 await loadSidebar();
-
+alert( "Step 2" )
 await loadTopbar();
-
+alert( "Step 3" )
 await loadFooter();
-
+alert( "Step 4" )
 navigate("dashboard");
 }
 catch(e){
 alert(e.message);
-
 window.location.href =
 "login.html";
 }
