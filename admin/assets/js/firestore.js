c1bcd40db290b2bc8ce4f73fc228d152f1e0feff
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 16:59:12 +0100

    Update firestore.js

--- admin/assets/js/firestore.js
+++ admin/assets/js/firestore.js
@@ -3,7 +3,7 @@ from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import { getAuth }
 from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
+ 
 import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
