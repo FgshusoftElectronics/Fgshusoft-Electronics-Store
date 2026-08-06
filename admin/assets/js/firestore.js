0355432e0f2f859d1faf19b9636728328503be39
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 13:35:12 +0100

    Create firestore.js

---
+++ admin/assets/js/firestore.js
@@ -0,0 +1,14 @@
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyD2U07u1C-gxdg2wk0Q9LUDgv958u8fN8Y",
+  authDomain: "max-electronics-69ba2.firebaseapp.com",
+  projectId: "max-electronics-69ba2",
+  storageBucket: "max-electronics-69ba2.firebasestorage.app",
+  messagingSenderId: "974351944204",
+  appId: "1:974351944204:web:19131c9178499e51987c29"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
