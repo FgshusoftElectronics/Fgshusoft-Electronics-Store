5987527ed6ed60784705edbdc8a638ccd1722c9d
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 13:57:21 +0100

    Update product-service.js

--- admin/assets/js/product-service.js
+++ admin/assets/js/product-service.js
@@ -1,113 +1,38 @@
 import {
-
 collection,
-
 addDoc,
-
 getDocs,
-
 deleteDoc,
-
 doc,
-
 serverTimestamp
-
 }
 from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import {db} from "./firebase.js";
 
-import {
-db
-}
-from "./firebase.js";
-
-
-
-
-
-const productsRef =
-collection(
-db,
-"products"
-);
-
-
-
-
+const productsRef = collection( db, "products" );
 
 // GET PRODUCTS
-
 export async function getProducts(){
-
-
-const snapshot =
-await getDocs(
-productsRef
-);
-
-
-
-return snapshot.docs.map(
-doc=>({
-
-id:doc.id,
-
-...doc.data()
-
-})
-
-);
-
-
+const snapshot = await getDocs( productsRef );
+return snapshot.docs.map( doc=>({ id:doc.id, ...doc.data()}) );
 }
-
-
-
-
 
 // ADD PRODUCT
 
-export async function addProduct(
-product
-){
-
-
-return await addDoc(
-productsRef,
+export async function addProduct( product ){
+return await addDoc( productsRef,
 {
-
 ...product,
-
 createdAt:
 serverTimestamp()
-
 }
-
 );
-
-
 }
-
-
-
-
 
 // DELETE PRODUCT
-
-export async function removeProduct(
-id
+export async function removeProduct( id
 ){
-
-
-await deleteDoc(
-doc(
-db,
-"products",
-id
-)
-
-);
-
-
+await deleteDoc( doc( db, "products", id ) );
 }
