8c236c40dc263f6042f3199e3b0e07e1db764d08
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 12:58:40 +0100

    Create profile.js

---
+++ admin/assets/js/profile.js
@@ -0,0 +1,8 @@
+import { auth }from "./firebase.js";
+
+auth.onAuthStateChanged( user=>{
+const email =document.getElementById("adminEmail");
+if(email && user){
+email.innerHTML = user.email;
+}
+});
