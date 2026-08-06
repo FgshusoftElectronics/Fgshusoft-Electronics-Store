d61eb141591c60f4faf6837fa9a873b46b45e962
FgshusoftElectronics <fgshusoft237@gmail.com>
2026-08-06 13:38:42 +0100

    Create product-service.js

---
+++ admin/assets/js/product-service.js
@@ -0,0 +1,113 @@
+import {
+
+collection,
+
+addDoc,
+
+getDocs,
+
+deleteDoc,
+
+doc,
+
+serverTimestamp
+
+}
+from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+import {
+db
+}
+from "./firebase.js";
+
+
+
+
+
+const productsRef =
+collection(
+db,
+"products"
+);
+
+
+
+
+
+// GET PRODUCTS
+
+export async function getProducts(){
+
+
+const snapshot =
+await getDocs(
+productsRef
+);
+
+
+
+return snapshot.docs.map(
+doc=>({
+
+id:doc.id,
+
+...doc.data()
+
+})
+
+);
+
+
+}
+
+
+
+
+
+// ADD PRODUCT
+
+export async function addProduct(
+product
+){
+
+
+return await addDoc(
+productsRef,
+{
+
+...product,
+
+createdAt:
+serverTimestamp()
+
+}
+
+);
+
+
+}
+
+
+
+
+
+// DELETE PRODUCT
+
+export async function removeProduct(
+id
+){
+
+
+await deleteDoc(
+doc(
+db,
+"products",
+id
+)
+
+);
+
+
+}
